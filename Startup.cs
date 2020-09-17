f4bb967062019f73ef9f035d9ffa8374066af42e
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-09-17 21:50:08 +0300

    Fixed decimal separator issue

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace RazorPagesMovie
 {
@@ -56,6 +58,16 @@ namespace RazorPagesMovie
             {
                 endpoints.MapRazorPages();
             });
+
+            var defaultCulture = new CultureInfo("fi-FI");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
         }
     }
 }
